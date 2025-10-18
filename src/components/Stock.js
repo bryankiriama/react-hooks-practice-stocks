16d08c00a08cdf9522f62c7fdc4e83f40bdb40ae
bryankiriama <bryankiriama@gmail.com>
2025-10-18 08:44:50 +0300

    created Flatiron Exachange

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,15 +1,15 @@
 import React from "react";
+import Stock from "./Stock";
 
-function Stock() {
+function StockContainer({ stocks, buyStock }) {
   return (
     <div>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
-        </div>
-      </div>
+      <h2>Stocks</h2>
+      {stocks.map((stock) => (
+        <Stock key={stock.id} stock={stock} buyStock={buyStock} />
+      ))}
     </div>
   );
 }
-export default Stock;
+
+export default StockContainer;
